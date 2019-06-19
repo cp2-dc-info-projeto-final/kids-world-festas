277e5cfb01442c6a2b69fb470c471512b0cbe665
lab lab <labcaxias@labdc.zentyal>
2019-06-19 08:29:03 -0300

    script de banco de dados

---
+++ schema-create.sql
@@ -0,0 +1,20 @@
+USE kids_world_festas;
+
+CREATE TABLE usuario(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    senha VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE administrador(
+    id INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY(id) REFERENCES usuario(id)
+);
+
+CREATE TABLE cliente(
+	id INT NOT NULL PRIMARY KEY,
+    cpf VARCHAR(11) NOT NULL,
+    telefone VARCHAR(11) NOT NULL,
+    FOREIGN KEY(id) REFERENCES usuario(id)
+);
