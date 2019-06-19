fce7ee2dee857837ced36d508262e99f164da026
lab lab <labcaxias@labdc.zentyal>
2019-06-19 11:47:51 -0300

    Atualização do script do banco de dados

--- schema-create.sql
+++ schema-create.sql
@@ -19,10 +19,38 @@ CREATE TABLE cliente(
     FOREIGN KEY(id) REFERENCES usuario(id)
 );
 
-
-CREATE TABLE locacao(
-    id INT NOT NULL PRIMARY KEY,
-    data DATE
+CREATE TABLE endereco(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    logradouro VARCHAR(100),
+    numero VARCHAR(10),
+    cep VARCHAR(7),
+    complemento VARCHAR(100),
+    cidade VARCHAR(50),
+    UF VARCHAR(2)
 );
 
-CREATE TABLE 
+CREATE TABLE locacao(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    id_endereco INT NOT NULL,
+    data DATE NOT NULL,
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id),
+    FOREIGN KEY(id_endereco) REFERENCES endereco(id)
+);
+
+CREATE TABLE produto(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    descricao VARCHAR(200),
+    preco DOUBLE NOT NULL
+);
+
+CREATE TABLE locacao_produto(
+	id_locacao INT,
+    id_produto INT,
+    preco_pago DOUBLE,
+    quantidade INT,
+    PRIMARY KEY(id_locacao, id_produto),
+    FOREIGN KEY(id_locacao) REFERENCES locacao(id),
+    FOREIGN KEY(id_produto) REFERENCES produto(id)
+);
