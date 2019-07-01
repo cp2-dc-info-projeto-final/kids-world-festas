3055b572e40f73dc19738999cf79b296f4c3e169
Emanuel Pires <42145525+Ueitom@users.noreply.github.com>
2019-07-01 07:17:10 -0300

    Update schema-create.sql

--- site/banco-de-dados/schema-create.sql
+++ site/banco-de-dados/schema-create.sql
@@ -33,7 +33,7 @@ CREATE TABLE locacao(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_cliente INT NOT NULL,
     id_endereco INT NOT NULL,
-    data DATE NOT NULL,
+    ´data´ DATE NOT NULL,
     FOREIGN KEY(id_cliente) REFERENCES cliente(id),
     FOREIGN KEY(id_endereco) REFERENCES endereco(id)
 );
