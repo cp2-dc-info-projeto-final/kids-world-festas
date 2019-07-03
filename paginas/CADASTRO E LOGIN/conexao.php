0369df8b3deae885901de7c72370035494779dc0
kah750 <kah750@gmail.com>
2019-07-03 08:45:13 -0300

    Conexão do cadastro

--- paginas/CADASTRO E LOGIN/conexao.php
+++ paginas/CADASTRO E LOGIN/conexao.php
@@ -18,13 +18,48 @@
         die("Erro" . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO usuario (nome, email, senha) VALUES
-            ('$nome', '$email', '$senha')";
-     
-     $sql = "INSERT INTO cliente (telefone, cpf) VALUES
-            ('$telefone', '$cpf')";
+    // check email
+    $sql_select_id = "SELECT email FROM usuario WHERE email='$email'";
+    
+    $result = mysqli_query($link, $sql_select_id);
 
-    if(mysqli_query($link, $sql)){
+    $count_email = 0;
+    // mysql fetch busca uma linha ou retorna falso quando não há
+    while ($row = mysqli_fetch_array($result)) {
+        $count_email++;
+       // echo "$id_usuario";
+    }
+
+    if ($count_email > 0) {
+        die("E-mail não disponível");
+    }
+
+    // insert usuario
+    $sql_insert_usuario = "INSERT INTO usuario (nome, email, senha) VALUES
+            ('$nome', '$email', '$senha')";
+
+    if(mysqli_query($link, $sql_insert_usuario)){
+        echo "Cadastro Concluído <br/>";
+    } else{
+        echo "Erro $sql. " . mysqli_error($link);
+    }
+
+    // select pegando o id inserido, filtrando WHERE com email
+    $sql_select_id = "SELECT id FROM usuario WHERE email='$email'";
+
+    $result = mysqli_query($link, $sql_select_id);
+
+    $id_usuario = null;
+    while ($row = mysqli_fetch_array($result)) {
+        $id_usuario = $row[0];
+       // echo "$id_usuario";
+    }
+
+    // insert cliente
+    $sql_insert_cliente = "INSERT INTO cliente (id, telefone, cpf) VALUES
+            ($id_usuario, '$telefone', '$cpf')";
+    
+    if(mysqli_query($link, $sql_insert_cliente)){
         echo "Cadastro Concluído";
     } else{
         echo "Erro $sql. " . mysqli_error($link);
--- site/banco-de-dados/schema-create.sql
+++ site/banco-de-dados/schema-create.sql
@@ -3,7 +3,7 @@ USE kids_world_festas;
 CREATE TABLE usuario(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
-    email VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
     senha VARCHAR(16) NOT NULL
 
 );
@@ -15,7 +15,7 @@ CREATE TABLE administrador(
 
 CREATE TABLE cliente(
     id INT NOT NULL PRIMARY KEY,
-    cpf VARCHAR(11) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     telefone VARCHAR(11) NOT NULL,
     FOREIGN KEY(id) REFERENCES usuario(id)
 );
