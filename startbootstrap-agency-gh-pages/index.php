206e595f775b2020b51eadd23ad46a62305a9f40
labcaxias <labcaxias@CDCLAB_07.labdc.zentyal>
2019-09-24 13:17:56 -0300

    login no index

--- startbootstrap-agency-gh-pages/index.php
+++ startbootstrap-agency-gh-pages/index.php
@@ -70,42 +70,33 @@
   </header>
 
   <!-- Services -->
-  <section class="page-section" id="services">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center">
-          <h2 class="section-heading text-uppercase">Services</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-      </div>
-      <div class="row text-center">
-        <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">E-Commerce</h4>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">Responsive Design</h4>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div class="col-md-4">
-          <span class="fa-stack fa-4x">
-            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">Web Security</h4>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-      </div>
-    </div>
-  </section>
+  <section class="bg-light page-section" id="services">
+        <center>
+            <form action="loginCtrl.php" method="POST">
+                <h2>Login</h2>
+                <p>
+                    <label for="email">E-mail:</label>
+                    <input type="text" placeholder="Digite seu e-mail" name="email" id="email" value="">
+                </p>
+                <p>
+                    <label for="senha">Senha:</label>
+                    <input type="password" placeholder="Digite sua senha" name="senha" id="senha" value="">
+                </p>
+                <input type="submit" value="Entrar">
+                <?php
+                    session_start();
+                    if(array_key_exists('erro', $_SESSION) == true){
+                        $erro = $_SESSION["erro"];
+                        echo "<br><b>$erro</br>";
+                        session_unset();
+                    }
+                ?>
+                <p>
+                        <a href="./cadastro.php">Cadastre-se</a>
+                </p>
+            </form>
+        </center>
+                  </section>
 
   <!-- Portfolio Grid -->
   <section class="bg-light page-section" id="portfolio">
