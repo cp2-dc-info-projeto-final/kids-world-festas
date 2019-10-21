ee7e85fd2b0b56b7eb3c9c05dd8170edfd803295
Emanuel Pires <ueitomep@gmail.com>
2019-10-21 08:04:08 -0300

    página do administrador
    pagina do administrador

--- startbootstrap-agency-gh-pages/login/loginView.php
+++ startbootstrap-agency-gh-pages/login/loginView.php
@@ -1,51 +1,65 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+   
+    <title>Cadastro</title>
+    <link href="css/agency.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/main.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/agency.min.css" media="screen" />
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+
+    <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>     
 </head>
 
-<body>
-    <div class="row">
-        <div class="col offset-s4 s4">
-            <div class="card light-blue lighten-3">
-                <form action="./login/loginCtrl.php" method="POST">
-                    <h2>Login</h2>
-                        <div class="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                            <label for="first_name">First Name</label>
-                        </div>
-                        <p>
-                            <label for="email">E-mail:</label>
-                            <input type="text" placeholder="Digite seu e-mail" name="email" id="email" value="">
-                        </p>
-                        <p>
-                            <label for="senha">Senha:</label>
-                            <input type="password" placeholder="Digite sua senha" name="senha" id="senha" value="">
-                        </p>
-                        <input class="btn" type="submit" value="Entrar">
-                        <?php
-                                        session_start();
-                                        if(array_key_exists('erro', $_SESSION) == true){
-                                            $erro = $_SESSION["erro"];
-                                            echo "<br><b>$erro</br>";
-                                            session_unset();
-                                        }
-                                    ?>
-                        <p>
-                            <a style="color: #fff59d" href="./cadastro/cadastroView.php">Cadastre-se</a>
-                        </p>
-                </form>
-            </div>
-
-
-        </div>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
+<body id= "cadastro">
+    
+    <center>
+        <!-- Estilizar Formulário -->
+        <section class="bg-light page-section" id="contact">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-12 text-center">
+                      <h2 class="section-heading text-uppercase">Login</h2>
+                      <h3 class="section-subheading text-muted">Seja Bem vindo novamente.</h3>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                        <div class="row">
+                          <div class="col-md-6">
+                            <div class="form-group">
+                             <input class="form-control" id="email" type="text" placeholder="Digite seu email *" required="required" data-validation-required-message="Please enter your name.">
+                              <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                               <input class="form-control" id="CPF" type="text" placeholder="Digite sua senha *" required="required" data-validation-required-message="Please enter your email address.">
+                              <p class="help-block text-danger"></p>
+                            </div>
+            
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                              <div id="success"></div>
+                              <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Entrar</button>
+                            </div>
+                          </div>
+            
+            <?php
+                session_start();
+                if(array_key_exists('erro', $_SESSION) == true){
+                    $erro = $_SESSION["erro"];
+                    echo "<br><b>$erro</br>";
+                    session_unset();
+                }
+            ?>
+            <p>
+                    <a href="./cadastro/cadastroView.html">Cadastre-se</a>
+            </p>
+        </form>
+    </center>  
+<!-- Contact form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script> 
 </body>
-
 </html>
