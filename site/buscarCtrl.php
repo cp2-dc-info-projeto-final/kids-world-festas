81037f0e0e7a0546c225592209754838a5abb997
Emanuel Pires <ueitomep@gmail.com>
2019-11-15 12:38:22 -0300

    Começando, data retroativa

--- site/buscarCtrl.php
+++ site/buscarCtrl.php
@@ -5,7 +5,10 @@
     function buscarProdutosCtrl($dia) {
         if (is_null($dia))
             return [];
-
+            
+        // if ($dia < date('Y-m-d') and $dia != [])
+        //     echo "esta busca não é permitida";
+        //     die;
         return buscarProdutos($dia);
     }
 ?>
