6ae8ab39946d7db262fc1026b88a472a86d94182
leticiabarrosoo <leticiabarroso4@gmail.com>
2019-11-21 16:23:54 -0300

    Cadastro - FUNCIONAL

--- site/cadastro/cadastroCtrl.php
+++ site/cadastro/cadastroCtrl.php
@@ -1,37 +1,29 @@
 <?php
-require "Cadastro_Model.php";
+session_start();
+require "cadastroModel.php";
 $nome =addslashes($_POST['nome']);
 $email =addslashes($_POST['email']);
+$telefone =addslashes($_POST['telefone']);
+$cpf =addslashes($_POST['cpf']);
 $senha = addslashes($_POST['senha']);
-$confirmasenha=addslashes($_POST['confirmasenha']);
-session_start();
+$confirmarsenha =addslashes($_POST['confirmarsenha']);
+
+
 if ($senha != $confirmasenha) {    
     $erro = "<p style='color:red;'>As senhas não coincidem.</p>";        
    $_SESSION['erro']=$erro;
-   header ("Location:Cadastro_View.php");
+   header ("Location:cadastroView.php");
     exit();
 }
-if ($_POST['select1']==$_POST['select2'] || $_POST['select2']==$_POST['select3'] || $_POST['select1']==$_POST['select3'] 
-|| $_POST['select4']==$_POST['select1'] || $_POST['select4']==$_POST['select2'] || $_POST['select4']==$_POST['select3'] ){
-    $erro ="<p style='color:red;'>Por favor, insira 4 gêneros diferentes.</p>";
-    $_SESSION['erro']=$erro;
-    header ("Location:Cadastro_View.php");
-    exit();
-}
-    $resposta=Cadastra_Usuario($nome, $email, $senha);
+
+    $resposta=Cadastra_Usuario($nome, $email, $senha, $telefone, $cpf);
 if ($resposta==false){
     $erro="<p style='color:red;'>E-mail indisponível</p>";
     $_SESSION["erro"]= $erro;
-    header ("Location:Cadastro_View.php");
+    header ("Location:cadastroView.php");
     exit();
     
-} else {
-    session_unset();
-    foreach ($generos as $genero) {
-        if ($_POST['select1']==$genero['id'] || $_POST['select2']==$genero['id'] || $_POST['select3']==$genero['id'] || $_POST['select4']==$genero['id']) {
-                
-            Usuario_Genero($resposta, $genero['id']);
-        }
-    }
-    header("Location:../Login/Login_View.php");
+} 
+else {
+    header("Location:../Login/loginVieww.php");
 }
--- site/cadastro/cadastroModel.php
+++ site/cadastro/cadastroModel.php
@@ -1,43 +1,39 @@
 <?php
- function Cadastra_Usuario($nome, $email, $senha){
-    include_once "../conexao.php";
-  // conexão
-  $link=getConnection();
+
+ require "../ConnectionFactory.php";
+ function cadastrarUsuario($nome, $email, $senha, $telefone, $cpf) {
+     // conexão
+     $link = getConnection();
 
   // check connection
   if($link === false){
       die("Erro na conexão com o banco de dados." . mysqli_connect_error());
   }
     $sql="SELECT id FROM usuario WHERE email='$email'";
-    $result=mysqli_query($conn, $sql);
+    $result=mysqli_query($link, $sql);
     
     $hash= password_hash($senha, PASSWORD_DEFAULT);
     if (mysqli_num_rows($result)>0){
         return false;
     } 
-    $sql="INSERT INTO usuario (nome, email, senha, moderador) VALUES ('$nome', '$email', '$hash', false)";
-    if (!mysqli_query($conn, $sql)) {
-        die("Erro ao efetuar o cadastro" . mysqli_error($conn));
+    $sql="INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$hash')";
+    if (!mysqli_query($link, $sql)) {
+        die("Erro ao efetuar o cadastro" . mysqli_error($link));
+
+      
         
     } else {
-        $id=mysqli_insert_id($conn);
-        mysqli_close($conn);
+        $id=mysqli_insert_id($link);
+        mysqli_close($link);
         return $id;
     }
     
   
  }
- function Usuario_Genero($id_usuario, $id_genero){
-    include_once "../conexao.php";
-    $conn=get_connection();
-   
-       if($conn===false){
-           die("Falha na conexão". mysqli_connect_error($conn));
-    }
-    $sql="INSERT INTO genero_usuario(id_usuario, id_genero) VALUES ($id_usuario, $id_genero)";
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
- }
+
+
+
+
  function Limpa($id){
     include_once "../conexao.php";
     $conn=get_connection();
@@ -45,8 +41,5 @@
        if($conn===false){
            die("Falha na conexão". mysqli_connect_error($conn));
     }
-    $sql="DELETE FROM genero_usuario WHERE id_usuario=$id";
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
  }
  ?>
