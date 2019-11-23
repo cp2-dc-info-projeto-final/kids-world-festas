c7a07a9c0cb333d89cbe22d89ef60d6f8aecabb0
leticiabarrosoo <leticiabarroso4@gmail.com>
2019-11-23 13:11:21 -0300

    Login adm funcionando

--- documentacao/src/site/login/loginCtrl.php
+++ documentacao/src/site/login/loginCtrl.php
@@ -1,23 +1,33 @@
 <?php
-
+    session_start();
     require "loginModelo.php";
-
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     
-    session_start();
-
-    if ($usuario = logar($email, $senha) != false) {
-        session_unset();
+    
+    if ($usuario = logar($email, $senha)) {
+        //print
+        //session_unset();
+        
         $_SESSION["nome"] = $usuario["nome"];
         $_SESSION["usuario_id"] = $usuario["id"];
-        header("Location: ../index.php");
+        
+        
+        $_SESSION["admin"]= administrador($email);
+        if ($_SESSION["admin"]==true){
+            header("Location: ../index-admin.php");
+            } else if ($_SESSION["admin"]==false){
+                header("Location: ../index.php");
+            }
+    
+       
         exit();
     }
     else {
+        
         $erro = "Login ou senha incorretos";
         $_SESSION["erro"] = $erro;
-        header("Location: loginVieww.php");
+        header("Location: loginView.php");
         exit();
     }
 ?>
--- documentacao/src/site/login/loginModelo.php
+++ documentacao/src/site/login/loginModelo.php
@@ -1,21 +1,20 @@
 <?php
-
     require "../ConnectionFactory.php";
-
     function logar($email, $senha) {
-
         $connection = getConnection();
-
         $sql_select_usuario = "SELECT id, senha, nome FROM usuario WHERE email='$email'";
-
+        //print_r($sql_select_usuario);
         $result = mysqli_query($connection, $sql_select_usuario);
-        
+        //print_r($result);
         if (mysqli_num_rows($result) > 0) {
             // output data pf each row
+            //print_r($result);die();
             while($row = mysqli_fetch_assoc($result)) {
-                $hash = $row["senha"];
                 
+                $hash = $row["senha"];
+                //
                 if($senha == $hash) {
+                    //print_r('$row');die();
                     return $row;
                 } else {
                     return false;
@@ -26,4 +25,29 @@
         }
         mysqli_close($connection);
     }
+    function administrador($email){
+        
+        $connection = getConnection();
+        if($connection === false){
+            die("Erro de Conexão" . mysqli_connect_error());
+        }
+        
+        $sql="SELECT * FROM usuario AS u JOIN  administrador AS a ON u.id = a.id  WHERE u.email = '$email'";
+        
+        $result=mysqli_query($connection, $sql);
+       
+        if (!$result){
+           
+            die(mysqli_error($connection));
+        }
+        if(mysqli_num_rows($result)==1){
+            
+            return true;
+        } 
+        else 
+            {
+            return false;
+            }
+    }
 ?>
+
