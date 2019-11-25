10496f567e88743094bb6765745cd4427983c1c7
Emanuel Pires <ueitomep@gmail.com>
2019-11-25 13:43:11 -0300

    Identação

--- site/login/loginCtrl.php
+++ site/login/loginCtrl.php
@@ -9,26 +9,19 @@
     
 
     if ($usuario = logar($email, $senha)) {
-        //print
-        //session_unset();
-        
         $_SESSION["nome"] = $usuario["nome"];
-        $_SESSION["usuario_id"] = $usuario["id"];
-        
-        
+        $_SESSION["usuario_id"] = $usuario["id"];        
         $_SESSION["admin"]= administrador($email);
+
         if ($_SESSION["admin"]==true){
             header("Location: ../index-admin.php");
             } else if ($_SESSION["admin"]==false){
                 header("Location: ../index.php");
+                exit();
             }
-    
-       
         exit();
     }
     else {
-        
-
         $erro = "Login ou senha incorretos";
         $_SESSION["erro"] = $erro;
         header("Location: loginView.php");
